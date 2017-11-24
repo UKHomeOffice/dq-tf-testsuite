b6350c0370b79e76c9aaa4ab503f964a6944f576
Nick Otter <nicholas.a.otter@gmail.com>
2017-11-24 15:28:16 +0000

    deleted tflint repetition

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ set -ex
 dockerd-entrypoint.sh &
 sleep 1
 docker run --rm -v $(pwd):/data -w /data hashicorp/terraform fmt --diff --check
-docker run --rm -v $(pwd):/data -w /data wata727/tflint tflint /data
+docker run --rm -v $(pwd):/data -w /data wata727/tflint /data
 docker run --rm -v $(pwd):/data -w /data quay.io/ukhomeofficedigital/tf-testrunner python -m unittest tests/*_test.py
