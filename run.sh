a563236dc6ff81e4318856fb1ba90dd1d0c84df7
Nick Otter <nicholas.a.otter@gmail.com>
2017-11-27 11:50:01 +0000

    tflint command updated

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ set -ex
 dockerd-entrypoint.sh &
 sleep 1
 docker run --rm -v $(pwd):/data -w /data hashicorp/terraform fmt --diff --check
-docker run --rm -v $(pwd):/data -w /data wata727/tflint /data
+docker run -ti --rm -v $(pwd):/data -w /data --entrypoint=/bin/sh wata727/tflint -c "tflint *.tf"
 docker run --rm -v $(pwd):/data -w /data quay.io/ukhomeofficedigital/tf-testrunner python -m unittest tests/*_test.py
