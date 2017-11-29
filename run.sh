c9808e02be78fa6b5fdf36182f65c0043e74afcd
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-11-29 09:34:47 +0000

    push aws secrets in to the test run

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ dockerd-entrypoint.sh &
 sleep 1
 docker run --rm -v $(pwd):/data -w /data hashicorp/terraform fmt --diff --check
 docker run --rm -v $(pwd):/data -w /data --entrypoint=/bin/sh wata727/tflint -c "tflint *.tf"
-docker run --rm -v $(pwd):/data -w /data quay.io/ukhomeofficedigital/tf-testrunner python -m unittest tests/*_test.py
+docker run --rm -v $(pwd):/data -w /data -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY quay.io/ukhomeofficedigital/tf-testrunner python -m unittest tests/*_test.py
